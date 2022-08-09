85e6f6a5a476f82f8036abea0d7d998ab48e561e
Thiago Mota <th.araujomota@gmail.com>
2022-08-09 15:46:05 -0300

    refac: fix faturamento word

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 SELECT 
     MIN(plans.plan_value) AS faturamento_minimo,
-    MAX(plans.plan_value) AS fautramento_maximo,
+    MAX(plans.plan_value) AS faturamento_maximo,
     ROUND(AVG(plans.plan_value), 2) AS faturamento_medio,
     SUM(plans.plan_value) AS faturamento_total
 FROM
