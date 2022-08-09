cbc32f9a8b7b2ce291d749bc4d1f90ec59cc0163
Thiago Mota <th.araujomota@gmail.com>
2022-08-09 16:26:49 -0300

    feat: adds query to show albums made by WalterPhoenix

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+SELECT 
+    artists.artist_name AS artista, albums.album_name AS album
+FROM
+    SpotifyClone.artists AS artists
+        INNER JOIN
+    SpotifyClone.albums AS albums ON albums.artist_id = artists.artist_id
+WHERE
+    artists.artist_name = 'Walter Phoenix'
+ORDER BY album
