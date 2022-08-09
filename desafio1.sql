2c296c66bb706c0a97bf19a077da52500cd4bac2
Thiago Mota <th.araujomota@gmail.com>
2022-08-09 00:10:25 -0300

    feat: adds users table

--- desafio1.sql
+++ desafio1.sql
@@ -7,3 +7,13 @@ CREATE TABLE SpotifyClone.plans (
     plan_name VARCHAR(100) NOT NULL,
     plan_value DECIMAL(3 , 2 )
 )  ENGINE=INNODB;
+
+CREATE TABLE SpotifyClone.users (
+    user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(100),
+    user_age INT NOT NULL,
+    plan_id INT NOT NULL,
+    subscription_date DATE,
+    FOREIGN KEY (plan_id)
+        REFERENCES plans (plan_id)
+)  ENGINE=INNODB;
