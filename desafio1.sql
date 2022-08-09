283b2bf5db67de3db8752f15cbcb3fd329c3109e
Thiago Mota <th.araujomota@gmail.com>
2022-08-09 00:46:13 -0300

    feat: insert user data

--- desafio1.sql
+++ desafio1.sql
@@ -66,3 +66,15 @@ VALUES('gratuito', 0.00),
 ('familiar', 7.99),
 ('universitÃ¡rio', 5.99),
 ('pessoal', 6.99)
+
+INSERT INTO SpotifyClone.users(user_name, user_age, plan_id, subscription_date)
+VALUES ('Thati', 23, 1, '2019-10-20'),
+  ('Cintia', 35, 2, '2017-12-30'),
+  ('Bill', 20, 3, '2019-06-05'),
+  ('Roger', 45, 4, '2020-05-13'),
+  ('Norman', 58, 4, '2017-02-17'),
+  ('Patrick', 33, 2, '2017-01-06'),
+  ('Vivian', 26, 3, '2018-01-05'),
+  ('Carol', 19, 3, '2018-02-14'),
+  ('Angelina', 42, 2, '2018-04-29'),
+  ('Paul', 46, 2, '2017-01-17');
