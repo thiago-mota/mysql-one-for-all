88dbe184dfb5fa9546477ca0fa845f6dc5a7a14f
Thiago Mota <th.araujomota@gmail.com>
2022-08-09 03:36:57 -0300

    feat: count cancoes, artistas and album amount

---
+++ desafio2.sql
@@ -0,0 +1,13 @@
+SELECT 
+    (SELECT 
+            COUNT(song_id)
+        FROM
+            SpotifyClone.songs) AS cancoes,
+    (SELECT 
+            COUNT(artist_id)
+        FROM
+            SpotifyClone.artists) AS artistas,
+    (SELECT 
+            COUNT(album_id)
+        FROM
+            SpotifyClone.albums) AS albuns;
